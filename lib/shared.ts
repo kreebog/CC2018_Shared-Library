d7e0db9b3165fa9a301596856ec0f3c10c00a7bb
James Dean <kreebog@gmail.com>
2018-06-26 14:19:12 -0400

    missed a checkin for v1.1.3

--- lib/shared.ts
+++ lib/shared.ts
@@ -2,6 +2,6 @@
 export { Logger } from './Logger';
 export { Maze } from './Maze';
 export { Cell } from './Cell';
-export { IMazeStub } from './Interfaces';
+export { IMazeStub, ICell, IMaze } from './Interfaces';
 export { Enums, DIRS, TAGS, GAME_RESULTS } from './Enums';
 export { Score } from './Score';
