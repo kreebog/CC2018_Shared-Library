cd4acb49f1e830f315498874c5aa860cc64ec3a4
James Dean <kreebog@gmail.com>
2018-07-10 11:07:33 -0400

    new game states ABORTED and ERROR

--- lib/Enums.ts
+++ lib/Enums.ts
@@ -1,18 +1,17 @@
-
 // Cardinal directions used for movement, exits, and other direction-based functions
 export enum DIRS {
-    NONE = 0,    
+    NONE = 0,
     NORTH = 1,
     SOUTH = 2,
     EAST = 4,
     WEST = 8
 }
 
-// Tags for cells, avatar, item, and other functions 
-export enum TAGS { 
+// Tags for cells, avatar, item, and other functions
+export enum TAGS {
     NONE = 0,
     START = 1,
-    FINISH = 2, 
+    FINISH = 2,
     PATH = 4,
     CARVED = 8
 }
@@ -33,7 +32,9 @@ export enum GAME_STATES {
     IN_PROGRESS = 2,
     WAIT_BOT = 4,
     WAIT_TEAM = 8,
-    FINISHED = 16
+    FINISHED = 16,
+    ABORTED = 32,
+    ERROR = 64
 }
 
 // enumeration of possible actions
@@ -51,7 +52,7 @@ export class Enums {
     private static instance: Enums;
 
     // Private constructor to enforce singleton pattern
-    private constructor() { }
+    private constructor() {}
 
     /**
      * Singleton pattern
@@ -65,9 +66,9 @@ export class Enums {
     }
 
     /**
-     * Returns comma-delimited string of the selected (bitwise) values within 
-     * the given enumeration.  
-     * 
+     * Returns comma-delimited string of the selected (bitwise) values within
+     * the given enumeration.
+     *
      * @param targetEnum - Only works with bitwise enumerations!
      * @param selectedBits - Number representing the selected bits
      */
@@ -79,7 +80,7 @@ export class Enums {
                 let bitVal: number = parseInt(dir);
                 if (!!(bitVal & selectedBits)) {
                     let stringVal: string = (<any>targetEnum)[bitVal];
-                    ret += (ret.length == 0 ? stringVal : ', ' + stringVal);
+                    ret += ret.length == 0 ? stringVal : ', ' + stringVal;
                 }
             }
         }
@@ -89,9 +90,9 @@ export class Enums {
     }
 
     /**
-     * Returns string array of the selected (bitwise) values within 
-     * the given enumeration.  
-     * 
+     * Returns string array of the selected (bitwise) values within
+     * the given enumeration.
+     *
      * @param targetEnum - Only works with bitwise enumerations!
      * @param selectedBits - Number representing the selected bits
      */
@@ -109,7 +110,7 @@ export class Enums {
         }
 
         if (ret.length == 0) ret.push('NONE');
-        
+
         return ret;
     }
 }
